a3ea597ef094f52b4d9e48b6f0b724f7be47f7b2
sommrook-42maru <sommrook@gmail.com>
2023-05-12 08:59:43 +0900

    Response Body -> String, Json

---
+++ springmvc/src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -0,0 +1,59 @@
+package hello.springmvc.basic.response;
+
+import hello.springmvc.basic.HelloData;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+// class단위에 ResponseBody를 붙이면 메소드에서 생략해도 된다.
+// RestController를 사용하면 Controller, ResponseBody 생략 가능 -> 다른 형태의 response는 skip한다.
+@Slf4j
+//@Controller
+//@ResponseBody
+@RestController
+public class ResponseBodyController {
+
+    @GetMapping("/response-body-string-v1")
+    public void responseBodyV1(HttpServletResponse response) throws IOException {
+        response.getWriter().write("ok");
+    }
+
+    @GetMapping("/response-body-string-v2")
+    public ResponseEntity<String> responseBodyV2(){
+        return new ResponseEntity<>("ok", HttpStatus.OK);
+    }
+
+//    @ResponseBody
+    @GetMapping("/response-body-string-v3")
+    public String responseBodyV3(){
+        return "ok";
+    }
+
+//    @ResponseBody
+    @GetMapping("/response-body-json-v1")
+    public ResponseEntity<HelloData> responseBodyJsonV1(){
+        HelloData helloData = new HelloData();
+        helloData.setUsername("chloe");
+        helloData.setAge(30);
+
+        return new ResponseEntity<>(helloData, HttpStatus.OK);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+//    @ResponseBody
+    @GetMapping("/response-body-json-v2")
+    public HelloData responseBodyJsonV2(){
+        HelloData helloData = new HelloData();
+        helloData.setUsername("chloe");
+        helloData.setAge(30);
+        return helloData;
+    }
+}
